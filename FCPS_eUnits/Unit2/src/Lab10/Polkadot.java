647c0f6cbaa64bfe9e090d3e5982500d1035fcbb
KamuiFan <fanyunxiang65@gmail.com>
2025-04-27 23:27:24 +0800

    兩顆紅球跑

--- FCPS_eUnits/Unit2/src/Lab10/Polkadot.java
+++ FCPS_eUnits/Unit2/src/Lab10/Polkadot.java
@@ -35,15 +35,15 @@
       }
       public double getY()      
       { 
-         //_______________
+         return myY;
       }
       public double getDiameter() 
       { 
-         //_______________
+         return myDiameter;
       }
       public Color getColor() 
       { 
-         //_______________
+         return myColor;
       }
       public double getRadius() 
       { 
@@ -54,9 +54,9 @@
       {
          myX = x;
       } 
-      public void setY(/*_________*/)
-      {
-         //_______________
+      public void setY(double y)
+      { 
+         myY = y;
       } 
       public void setColor(Color c)
       {
