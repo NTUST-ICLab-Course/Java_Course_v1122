eb86f54b16bf70d387880f89c84baa1961686bc8
KamuiFan <fanyunxiang65@gmail.com>
2025-04-28 19:23:15 +0800

    80%
    listener1結束

--- FCPS_eUnits/Unit2/src/Lab13/ProjectPanel.java
+++ FCPS_eUnits/Unit2/src/Lab13/ProjectPanel.java
@@ -8,14 +8,59 @@ public class ProjectPanel extends JPanel
    private BufferedImage myImage;
    private Graphics2D myBuffer;
    private Timer t1, t2;
-   private int xPos;
+   private int xPos=250;
+   private int yPos = 150;
 
    public ProjectPanel()
    {
       xPos = 50;
       myImage = new BufferedImage(700, 500, BufferedImage.TYPE_INT_RGB);
       myBuffer = (Graphics2D)myImage.getGraphics();
-      t1 = new Timer(100, new Listener1());
-      // ...
+      t1 = new Timer(100, new Listener_1());
+      
+      t1.start();
+      t2 = new Timer(3000,new Listener2_());
    }
+   
+   public void paintComponent(Graphics g)
+      {
+         g.drawImage(myImage,0,0,getWidth(),getHeight(),null);
+      }
+   private class Listener_1 implements ActionListener
+      {
+            public void actionPerformed(ActionEvent e) {
+                int xPoints[] = { xPos,xPos, xPos + 50,xPos, xPos - 50,xPos,xPos, xPos - 50, xPos + 50 };
+                int yPoints[] = { yPos, yPos + 50, yPos, yPos + 50, yPos, yPos + 50, yPos + 100, yPos + 100, yPos + 150 };
+            
+                myBuffer.setColor(Color.gray);
+                myBuffer.fillRect(0, 0, 700, 350);
+                myBuffer.setColor(Color.darkGray);
+                myBuffer.fillRect(0, 335, 700, 500);
+                myBuffer.drawImage(Thomas.getImage(), xPos - 40, 50, 75, 125, null);
+                myBuffer.setStroke(new BasicStroke(10.0f));
+                myBuffer.drawPolyline(xPoints, yPoints, 10);
+            
+                repaint();
+                xPos = xPos + 5;
+                if (xPos >= 200) {
+                    myBuffer.setFont(new Font("Monospaced", Font.BOLD, 24));
+                    myBuffer.drawString("騎颱風要搭捷運出門", 300, 150);
+                    myBuffer.drawOval(250, 80, 400, 100);
+                    repaint();
+                    t1.stop();
+                    t2.start();
+                }
+            }
+         
+      }
+      
+      private class Listener_2 implements ActionListener
+      {
+          public void actionPerformed(ActionEvent e)
+         {
+            t2.start();
+            myBuffer.setFont(new Font("Monospaced", Font.BOLD,24));
+            myBuffer.drawString()
+         }
+      }
 }
