6522969925f327b92b962d98dca2cb033c8ad4c3
KamuiFan <fanyunxiang65@gmail.com>
2025-05-05 23:48:23 +0800

    oo

--- FCPS_eUnits/Unit2/src/Lab15/KarelPanel.java
+++ FCPS_eUnits/Unit2/src/Lab15/KarelPanel.java
@@ -42,22 +42,60 @@
             myBuffer.setColor(Color.red);
             for(int x = 17; x < WIDTH; x += 30) //vertical lines
             {
-               myBuffer.drawLine(/*     */);
+               myBuffer.drawLine(x,0,x,HEIGHT);
             }
             for(int y = 9; y < HEIGHT; y += 28) //horizontal lines
             {
-               myBuffer.drawLine(/*     */);
+               myBuffer.drawLine(0,y,WIDTH,y);
             }
             myBuffer.fillRect(107, 121, 180, 56);
             myBuffer.setColor(Color.black);
             myBuffer.setFont(new java.awt.Font("SansSerif", java.awt.Font.BOLD, 20));
             myBuffer.drawString("Karel the Robot", 125, 155);
             
-         	/*************************************
-         	Calculate karel's xPos and yPos.
-         	At the end of each row, the dir changes.
-         	The dir controls which image to use.
-         	*************************************/
+            switch(dir){
+               case 0:
+                  xPos = xPos+30;
+                  break;
+               case 1:
+                  yPos = yPos-28;
+                  break;
+               case 2:
+                  xPos = xPos-30;
+                  break;
+               case 3:
+                  yPos = yPos+28;
+                  break;
+            }
+            
+            if(yPos >= 381){
+               if(dir != 0){
+                  xPos = 5;
+                  yPos = (388-myArray[0].getImage().getHeight(null));
+               }
+               dir = 0;
+            }
+            if(xPos >=395){
+               if(dir !=1){
+                  yPos = (386-myArray[1].getImage().getHeight(null));
+                  xPos = (392-myArray[1].getImage().getWidth(null));
+               }
+               dir = 1;
+            }
+            if(yPos <= -4){
+               if(dir != 2){
+                  xPos = (390 - myArray[2].getImage().getWidth(null));
+                  yPos = -4;
+               }
+               dir = 2;
+            }
+           if((xPos <= -3) && (dir!=0)){
+               if(dir != 3){
+                  xPos = 3;
+                  yPos = -3;
+               }
+               dir = 3;
+            }
             myBuffer.drawImage(myArray[dir].getImage(), xPos, yPos, null);
             
             repaint();
